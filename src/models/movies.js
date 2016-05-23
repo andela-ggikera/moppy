767e49744227a42d76b5020e299a965d081b1b40
andela-ggikera <githinji.gikera@andela.com>
2016-05-23 11:10:21 +0300

    (chore) Define a schema for the movie database

---
+++ src/models/movies.js
@@ -0,0 +1,25 @@
+// Persistence layer defining the movie schema
+var mongoose = require('mongoose');
+var generateId = require('./plugin/generateId');
+var movieSchema = new mongoose.Schema({
+    id: {
+        type: Number,
+        required: true,
+        index: {
+            unique: true
+        }
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    year: {
+        type: Number,
+        required: true
+    },
+    actors: [{
+        type: mongoose.Schema.ObjectId,
+        ref: 'Actor'
+    }]
+});
+movieSchema.plugin(generateId());
