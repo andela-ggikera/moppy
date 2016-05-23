73680a1784954d51db1e0bc1eae46eac470d64d6
andela-ggikera <githinji.gikera@andela.com>
2016-05-23 15:39:34 +0300

    (fix) Refactor generateId function to fix callback error

--- src/models/plugins/generateId.js
+++ src/models/plugins/generateId.js
@@ -2,7 +2,7 @@
 A generic plugin to autogenerate ids for model entities.
 */
 module.exports = function() {
-    var generateId = function(schema) {
+    return function generateId(schema) {
         schema.pre('validate', function(next, done) {
             var instance = this;
             // apply to any mongoose schema
@@ -11,15 +11,14 @@ module.exports = function() {
                 model.findOne().sort("-id").exec(function(err, maxInstance) {
                     if (err) { return done(err); }
                     else {
-                        var maxId = maxInstance.id || 0;
+                        var maxId = maxInstance ? maxInstance.id : 0;
                         instance.id = maxId + 1;
-                        done();
+                        next();
                     }
-                });
+                })
             } else {
                 done();
             }
         });
     };
-    return generateId;
 };
