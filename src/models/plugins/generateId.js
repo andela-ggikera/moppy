8f856b1f48c78467ea27d6626ec6f7d7943042aa
andela-ggikera <githinji.gikera@andela.com>
2016-05-23 11:23:01 +0300

    (chore) Add the id generator for the model entities

---
+++ src/models/plugins/generateId.js
@@ -0,0 +1,23 @@
+/*
+A generic plugin to autogenerate ids for model entities.
+*/
+module.exports = function() {
+    var generateId = function(next, done) {
+        var instance = this;
+        // apply to any mongoose schema
+        var model = instance.model(instance.constructor.modelName);
+        if (instance.id == null) {
+            model.findOne().sort("-id").exec(function(err, maxInstance) {
+                if (err) { return done(err); }
+                else {
+                    var maxId = maxInstance.id || 0;
+                    instance.id = maxId + 1;
+                    done();
+                }
+            });
+        } else {
+            done();
+        }
+    };
+    return generateId;
+};
